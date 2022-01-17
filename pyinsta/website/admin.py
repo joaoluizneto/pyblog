9c19aecff7f2ae006b4d12896795b873947925a0
joao_luiz_neto <joao_luiz@id.uff.br>
2022-01-17 04:09:50 -0300

    reaproveitando código

--- pyinsta/website/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models as mod
-
-model_classes = [cls for name, cls in mod.__dict__.items() if isinstance(cls, type)]
-
-# Register your models here.
-for model_class in model_classes: admin.site.register(model_class)
-
